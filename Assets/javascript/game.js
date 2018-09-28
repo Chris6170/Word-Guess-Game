90fd36915eea3da8fc3d28504f93d752c711670d
Fife <39037648+Chris6170@users.noreply.github.com>
2018-09-28 11:04:52 -0700

    New

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -43,8 +43,11 @@ var guesses = document.getElementById("guesses");
 var userInput = document.getElementById("userInput");
 var answerBox = document.getElementById("test");
 var button = document.getElementById("button")
-var answers = [ "blink182", "sum41", "", "greenday" ];
+var answers = [ "blink182", "sum41","greenday" ];
+var correctA = [];
 var randomAnswers = answers[Math.floor(Math.random()*answers.length)];
+
+
 
 console.log(randomAnswers);
 
@@ -54,17 +57,44 @@ for (i = 0, len = randomAnswers.length, text = ""; i < len; i++) {
     var i, len, text;
     text += "- " ;
 
+ 
+
     document.getElementById("test").innerHTML = text ;
+
+   
 }
      
  
   button.addEventListener("click", function(){
 
+    
+
     if(randomAnswers.indexOf(userInput.value) > -1){
 
-        console.log("yes");
+        correctA.push(userInput.value);
+        console.log(correctA);
 
-        document.getElementById("test").innerHTML = userInput.value;
+        text[userInput] = randomAnswers[userInput];
+        console.log(text);
+  
+
+        
+
+        
+        
+     
+        
+
+        
+
+
+        
+
+
+
+       
+
+       
 
 
 
