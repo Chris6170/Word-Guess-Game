eeb84ac2fd1abc0c1f8cfd37cae514d78806d69a
Fife <39037648+Chris6170@users.noreply.github.com>
2018-09-30 20:35:49 -0700

    done

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,41 +1,7 @@
 
 
-// var name = prompt("What is your name?");
 
 
-// alert("Hey " + name + ", how are you?");
-
-// var fruits = ["Apples", "Oranges","Bananas"];
-
-// alert(fruits);
-
-// alert(fruits[0]);
-
-// function sayHi(input){
-
-//    document.write("Hey " + input + ", time to put this work in!")
-// };
-
-
-
-// sayHi("Fife");
-
-
-
-    //    var header =   document.getElementById("header");
-
-    //    header.style.color = "blue";
-    //    header.style.backgroundColor = "red";
-
-    //    var rick =  document.getElementById("image");
-
-
-    //    rick.addEventListener('click', function(){
-
-
-    //     rick.style = "width:500px";})
-
-  
 
 
 
@@ -43,63 +9,174 @@ var guesses = document.getElementById("guesses");
 var userInput = document.getElementById("userInput");
 var answerBox = document.getElementById("test");
 var button = document.getElementById("button")
-var answers = [ "blink182", "sum41","greenday" ];
+var answers = ["blink182", "sum41", "greenday"];
 var correctA = [];
-var randomAnswers = answers[Math.floor(Math.random()*answers.length)];
-
-
-
+var inco = [];
+var randomAnswers = answers[Math.floor(Math.random() * answers.length)];
+randomAnswers = randomAnswers.split('');
 console.log(randomAnswers);
+var W = document.getElementById("wins");
+var L = document.getElementById("losses");
+var tries = 5;
+var win = 0;
+var lose = 0;
 
 
 
-for (i = 0, len = randomAnswers.length, text = ""; i < len; i++) {
-    var i, len, text;
-    text += "- " ;
-
- 
-
-    document.getElementById("test").innerHTML = text ;
-
-   
-}
-     
- 
-  button.addEventListener("click", function(){
-
+function begin (){
+    for (i = 0; i < randomAnswers.length; i++) {
+        var i
+    
+        correctA.push("- ");
+        answerBox.innerHTML = correctA.join("");
+        console.log(randomAnswers);
+    
+    
+    
+        
+    
+    }
     
 
-    if(randomAnswers.indexOf(userInput.value) > -1){
+}
 
-        correctA.push(userInput.value);
-        console.log(correctA);
+begin();
 
-        text[userInput] = randomAnswers[userInput];
-        console.log(text);
-  
+
+
+
+
+
+
+
+
+
+
+
+
+button.addEventListener("click", function () {
+
+    if ( tries === 1){
+        lose++
+        L.innerHTML = lose.toString();
+
+    }
+
+    for (var i = 0; i < randomAnswers.length; i++) {
+
+
+
+        if (correctA[i] === userInput.value) {
+
+            alert("You're only gonna get it right once Einstein");
+
+            userInput.value = "";
+
+
+
+
+
+
+
+        };
+
+        if (inco[i] === userInput.value) {
+
+
+            alert("I already told you that its wrong.........SMH!")
+            userInput.value = ""
+        };
+
+
+
+
+
+    }
+
+
+
+
+
+
+    for (var i = 0; i < randomAnswers.length; i++) {
+
+        if (userInput.value === "") {
+            break
+        }
+
+
+        if (randomAnswers[i] === userInput.value) {
+            correctA[i] = randomAnswers[i];
+            answerBox.innerHTML = correctA.join("");
+
+
+
+
+
+
+
+        }
+
+
+
+        if (randomAnswers.indexOf(userInput.value) === -1) {
+
+            alert("Close but..........actually not close at all.")
+             tries--;
+             alert(tries + " more and you're out.")
+            inco.push(userInput.value);
+
+            var wrong = document.createElement("li");
+            wrong.innerHTML = userInput.value;
+            guesses.appendChild(wrong);
+
+            userInput.value = "";
+            break
+
+
+
+
+
+        }
+
+        if (correctA.toString() === randomAnswers.toString()){
+
+            win++;
+            W.innerHTML = win.toString();
+
+            
+            answers[Math.floor(Math.random() * answers.length)];
+            answers.pop()
+            begin;
+            guesses.innerHTML = "";
+            answerBox.innerHTML = "";
+            userInput.value = "";
+
+            
+            
 
         
-
         
-        
-     
-        
-
-        
-
-
-        
-
-
+        }
 
        
 
-       
 
 
 
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
@@ -115,31 +192,27 @@ for (i = 0, len = randomAnswers.length, text = ""; i < len; i++) {
 
 
 
-  })
-
-
-
- 
-  
-
-  
 
 
 
 
 
 
- 
-
-     
-
-    
 
 
 
 
 
-    
+
+
+
+
+
+
+
+
+
+});
 
 
 
@@ -164,6 +237,69 @@ for (i = 0, len = randomAnswers.length, text = ""; i < len; i++) {
 
 
 
-    
 
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
